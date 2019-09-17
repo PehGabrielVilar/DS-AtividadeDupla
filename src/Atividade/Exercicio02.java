8d300147a126524f420721816755b6c0011dcb60
LABORATORIO_INFO <LABORATORIO_INFO@10.67.96.52>
2019-09-17 20:59:14 -0300

    Fazendo a Atividade01 - Exercicio02: 17/09

--- src/Atividade/Exercicio02.java
+++ src/Atividade/Exercicio02.java
@@ -6,11 +6,29 @@ public class Exercicio02 {
 
     public static void main(String[] args) {
         String aux = "";
-        double 
-        
-        aux = JOptionPane.showInputDialog(null, "");
-         = Double.parseDouble(aux);
-        aux = aux.replace(",", ".");
+        double sbruto = 0, prest = 0, porc = 0, res = 0, res1 = 0;
+
+        try {
+            aux = JOptionPane.showInputDialog(null, "Digite o seu salário bruto: ");
+            sbruto = Double.parseDouble(aux);
+            aux = aux.replace(",", ".");
+
+            aux = JOptionPane.showInputDialog(null, "Digite o valor da prestação do empréstimo: ");
+            prest = Double.parseDouble(aux);
+            aux = aux.replace(",", ".");
+
+            res = prest /(sbruto / 100);
+            
+            if (res >= 0 && res <= 30) {
+                JOptionPane.showMessageDialog(null, "Empréstimo pode ser concedido.");
+            }
+            if (res > 30) {
+                JOptionPane.showMessageDialog(null, "Empréstimo não pode ser concedido.");
+            }
+        } catch (NumberFormatException erro) {
+            JOptionPane.showMessageDialog(null, "Digite somente números !");
+        }
+
     }
 
 }
