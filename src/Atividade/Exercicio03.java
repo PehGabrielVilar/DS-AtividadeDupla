573cfb774bc11f9c4c433e34b306720bce8bc2df
Pedro Gabriel Vilar <pedro.santos589@etec.sp.gov.br>
2019-09-17 21:25:21 -0300

    Fazendo a Atividade01 - Exercicio03: 17/09

--- src/Atividade/Exercicio03.java
+++ src/Atividade/Exercicio03.java
@@ -3,9 +3,23 @@ package Atividade;
 import javax.swing.*;
 
 public class Exercicio03 {
+
     public static void main(String[] args) {
         String aux = "";
-        double raio = 0
+        double raio = 0, pi = 3.1416, resp = 0;
+
+        try {
+            aux = JOptionPane.showInputDialog(null, "Informe o tamanha do raio da circunferência: ");
+            raio = Double.parseDouble(aux);
+            aux = aux.replace(",", ".");
+
+            resp = 2 * pi * raio;
+
+            JOptionPane.showMessageDialog(null, "O tamanho do perímetro é: " + resp);
+        }catch (NumberFormatException erro) {
+            JOptionPane.showMessageDialog(null, "Digite somente números !");
+        }
+
     }
 
 }
