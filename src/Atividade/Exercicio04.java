6e36696cf4348f9e1d83496ca5ed158141c5a791
Pedro Gabriel Vilar <pedro.santos589@etec.sp.gov.br>
2019-09-17 22:03:21 -0300

    Fazendo a Atividade01 - Exercicio04: 17/09

--- src/Atividade/Exercicio04.java
+++ src/Atividade/Exercicio04.java
@@ -1,14 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Atividade;
 
-/**
- *
- * @author LABORATORIO_INFO
- */
+import javax.swing.*;
+
 public class Exercicio04 {
-    
+
+    public static void main(String[] args) {
+        String aux = "";
+        double num0 = 0, num1 = 0, num2 = 0, num3 = 0, resp = 0;
+
+        try {
+            
+            if (num0 >= 0 && num1 >= 0 && num2 >= 0 && num3 >= 0){
+            aux = JOptionPane.showInputDialog(null, "");
+            num0 = Double.parseDouble(aux);
+            aux = aux.replace(",", ".");
+
+            aux = JOptionPane.showInputDialog(null, "");
+            num1 = Double.parseDouble(aux);
+            aux = aux.replace(",", ".");
+
+            aux = JOptionPane.showInputDialog(null, "");
+            num2 = Double.parseDouble(aux);
+            aux = aux.replace(",", ".");
+
+            aux = JOptionPane.showInputDialog(null, "");
+            num3 = Double.parseDouble(aux);
+            aux = aux.replace(",", ".");
+
+            resp = num0 + num1 + num2 + num3;
+
+            JOptionPane.showMessageDialog(null, "A soma dos números é: " + resp);  
+            }
+
+        } catch (NumberFormatException erro) {
+            JOptionPane.showMessageDialog(null, "Digite somente números !");
+
+        }
+    }
 }
