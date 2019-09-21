b59dc4614219801ea34c59f35a9e7ad9b913305e
Pedro Gabriel Vilar <pedro.santos589@etec.sp.gov.br>
2019-09-21 15:04:29 -0300

    Fazendo a Atividade01 - Exercicio05: 21/09

--- src/Atividade/Exercicio05.java
+++ src/Atividade/Exercicio05.java
@@ -6,6 +6,12 @@ public class Exercicio05 {
     public static void main(String[] args) {
         String aux = "";
         int i = 5, j, k;
-        
+        j = --i;
+        k = i++;
+        i+= 10;
+        JOptionPane.showMessageDialog(null, "o valor de i: "+ i + 
+                "\n o valor de j: "+ j +
+                "\n o valor de k: "+ k +
+                "\n o valor de k: "+ k);
     }
 }
