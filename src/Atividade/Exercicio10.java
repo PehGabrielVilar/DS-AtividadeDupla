4183141452f6c9dbb10180920f862d7042166147
Pedro Gabriel Vilar <pedro.santos589@etec.sp.gov.br>
2019-09-22 14:45:18 -0300

    Fazendo a Atividade01 - Exercicio10: 22/09

--- src/Atividade/Exercicio10.java
+++ src/Atividade/Exercicio10.java
@@ -1,14 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Atividade;
 
-/**
- *
- * @author LABORATORIO_INFO
- */
+import javax.swing.*;
+
 public class Exercicio10 {
-    
+    public static void main(String[] args) {
+        String aux = "";
+        double tempo = 0, velocidade = 0, distancia = 0, litros = 0, km = 0;
+        try {
+            JOptionPane.showMessageDialog(null, "Digite somente números !\n"
+                    + "Exemplo: 2,30");
+
+            aux = JOptionPane.showInputDialog(null, "Quanto tempo foi gasto na viagem?");
+            aux = aux.replace(",", ".");
+            tempo = Double.parseDouble(aux);
+
+            aux = JOptionPane.showInputDialog(null, "Qual foi a velocidade média?");
+            aux = aux.replace(",", ".");
+            velocidade = Double.parseDouble(aux);
+
+        } catch (NumberFormatException erro) {
+            JOptionPane.showMessageDialog(null, "Digite somente números !");
+            System.exit(0);
+        }
+        distancia = tempo * velocidade;
+        litros = distancia / 12;
+
+        JOptionPane.showMessageDialog(null, "Foram gastos " + litros + " litros de combustível");
+
+    }
 }
