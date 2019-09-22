00ab42d74c9362e8738a179b4954de44f25dd548
Pedro Gabriel Vilar <pedro.santos589@etec.sp.gov.br>
2019-09-22 14:46:16 -0300

    Fazendo a Atividade01 - Exercicio12: 22/09

--- src/Atividade/Exercicio12.java
+++ src/Atividade/Exercicio12.java
@@ -1,14 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Atividade;
 
-/**
- *
- * @author LABORATORIO_INFO
- */
+import javax.swing.*;
+
 public class Exercicio12 {
-    
+    public static void main(String[] args) {
+         String aux = "";
+        int num = 0, ant = 0, suc = 0;
+        try {
+            aux = JOptionPane.showInputDialog(null, "Digite um número inteiro para receber seu antecessor e o seu sucessor");
+            num = Integer.parseInt(aux);
+
+            ant = num - 1;
+            suc = num + 1;
+        } catch (NumberFormatException erro) {
+            JOptionPane.showMessageDialog(null, "Digite somente números inteiros");
+        }
+        JOptionPane.showMessageDialog(null, "O antecessor é " + ant + " e o sucessor é " + suc);
+    }
 }
