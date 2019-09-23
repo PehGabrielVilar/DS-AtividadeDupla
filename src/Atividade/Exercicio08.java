5d4e5a5392701ebfea164a8ca958bd26830d23c1
Pedro Gabriel Vilar <pedro.santos589@etec.sp.gov.br>
2019-09-23 11:55:06 -0300

    Fazendo a Atividade01 - Exercicio08: 23/09

--- src/Atividade/Exercicio08.java
+++ src/Atividade/Exercicio08.java
@@ -6,15 +6,36 @@ public class Exercicio08 {
 
     public static void main(String[] args) {
         String aux = "";
-        double salariob = 0, salariol = 0, aulahora = 0, aulames = 0, desconto = 0;
+        double aulahora = 0, aulames = 0, sbruto = 0, sliquido = 0, desconto = 0;
         try {
             aux = JOptionPane.showInputDialog(null, "Digite o valor da hora de aula");
-            aux = aux.replace(",", ".");
             aulahora = Double.parseDouble(aux);
+            aux = aux.replace(",", ".");
 
             aux = JOptionPane.showInputDialog(null, "Digite o número de aulas dadas por mês");
-            aux = aux.replace(",", ".");
             aulames = Double.parseDouble(aux);
+            aux = aux.replace(",", ".");
+
+            sbruto = aulahora * aulames;
+
+            if (sbruto <= 1121.45) {
+                desconto = sbruto * 8 / 100;
+                sliquido = sbruto - desconto;
+            }
+            if (sbruto > 1121.45 && sbruto <= 1954.89) {
+                desconto = sbruto * 9 / 100;
+                sliquido = sbruto - desconto;
+            }
+            if (sbruto > 1954.89 && sbruto <= 2639.45) {
+                desconto = sbruto * 11 / 100;
+                sliquido = sbruto - desconto;
+            }
+            if (sbruto > 2639.45) {
+                desconto = sbruto * 11 / 100;
+                sliquido = sbruto - desconto;
+            }
+
+            JOptionPane.showMessageDialog(null, "O salário líquido é de: " + "R$ " + sliquido);
 
         } catch (NumberFormatException erro) {
             JOptionPane.showMessageDialog(null, "Digite somente números !");
@@ -22,20 +43,5 @@ public class Exercicio08 {
 
         }
 
-        salariob = (aulames * aulahora) * 4.50;
-        if (salariob <= 1751.81) {
-            desconto = (salariob * 8 / 100);
-            salariol = salariob - desconto;
-        }
-        if (salariob >= 1751.81 && salariob <= 2919.72) {
-            desconto = (salariob * 9 / 100);
-            salariol = salariob - desconto;
-        }
-        if (salariob >= 2919.73 && salariob <= 5839.45) {
-            desconto = (salariob * 11 / 100);
-            salariol = salariob - desconto;
-        }
-
-        JOptionPane.showMessageDialog(null, "O salário líquido é de: " + "R$ " + salariol);
     }
 }
