3cf4da4aa8991fd073bd9d5dd540df8ca2985783
Pedro Gabriel Vilar <pedro.santos589@etec.sp.gov.br>
2019-09-23 21:17:54 -0300

    Fazendo a Atividade01 - 23/09

--- src/Atividade/Exercicio13.java
+++ src/Atividade/Exercicio13.java
@@ -9,14 +9,14 @@ public class Exercicio13 {
         double idade = 0;
         int resp = 0;
 
-        for (int i = 1; i <=5; i++ ) {
+        for (int i = 1; i <=20; i++ ) {
             aux = JOptionPane.showInputDialog(null, "Digite a idade");
             idade = Double.parseDouble(aux);
-            
-        }
-       if (idade >= 18){
+          if (idade >= 18){
                resp++;   
+        }  
         }
+       
         JOptionPane.showMessageDialog(null,"A quantidade de maiores de idade é "+ resp  );
     }
 
